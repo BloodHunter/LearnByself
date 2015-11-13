68227ba71549a8e678d3b8175a4ed331332145b5
BloodHunter <15201414128@163.com>
2015-11-13 14:34:55 +0800

    just for test

---
+++ src/test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Simple_love on 2015/11/13.
+ */
+public class test {
+        public static void main(String[] args) {
+                System.out.println("Hello World!");
+        }
+}
